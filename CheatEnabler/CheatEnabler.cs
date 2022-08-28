f8836c4c8867881afb67ae82bb3f8d9f1d6d732e
Soar Qin <soarchin@gmail.com>
2022-08-28 10:58:03 +0800

    rename DevShortcuts to CheatEnabler and add more cheat functions

---
+++ CheatEnabler/CheatEnabler.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using HarmonyLib;
+
+namespace CheatEnabler;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class CheatEnabler : BaseUnityPlugin
+{
+    private new static readonly BepInEx.Logging.ManualLogSource Logger =
+        BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_NAME);
+
+    private bool _devShortcuts = true;
+    private bool _disableAbnormalChecks = true;
+    private bool _alwaysInfiniteResource = true;
+
+    private void Awake()
+    {
+        _devShortcuts = Config.Bind("General", "DevShortcuts", _devShortcuts, "enable DevMode shortcuts").Value;
+        _disableAbnormalChecks = Config.Bind("General", "DisableAbnormalChecks", _disableAbnormalChecks,
+            "disable all abnormal checks").Value;
+        _alwaysInfiniteResource = Config.Bind("General", "AlwaysInfiniteResource", _alwaysInfiniteResource,
+            "always infinite resource").Value;
+        if (_devShortcuts)
+        {
+            Harmony.CreateAndPatchAll(typeof(DevShortcuts));
+        }
+        if (_disableAbnormalChecks)
+        {
+            Harmony.CreateAndPatchAll(typeof(AbnormalDisabler));
+        }
+        if (_alwaysInfiniteResource)
+        {
+            Harmony.CreateAndPatchAll(typeof(AlwaysInfiniteResource));
+        }
+    }
+
+    private class DevShortcuts
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PlayerController), "Init")]
+        private static void PlayerControllerInit(PlayerController __instance)
+        {
+            var cnt = __instance.actions.Length;
+            var newActions = new PlayerAction[cnt + 1];
+            for (var i = 0; i < cnt; i++)
+            {
+                newActions[i] = __instance.actions[i];
+            }
+
+            var test = new PlayerAction_Test();
+            test.Init(__instance.player);
+            newActions[cnt] = test;
+            __instance.actions = newActions;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PlayerAction_Test), "GameTick")]
+        private static void PlayerAction_TestGameTick(PlayerAction_Test __instance)
+        {
+            __instance.Update();
+        }
+    }
+
+    private class AbnormalDisabler
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyBeforeGameSave")]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyOnAssemblerRecipePick")]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyOnGameBegin")]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyOnMechaForgeTaskComplete")]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyOnUnlockTech")]
+        [HarmonyPatch(typeof(AbnormalityLogic), "NotifyOnUseConsole")]
+        private static bool DisableAbnormalLogic()
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AbnormalityLogic), "InitDeterminators")]
+        private static bool DisableAbnormalDeterminators(ref Dictionary<int, AbnormalityDeterminator> ___determinators)
+        {
+            ___determinators = new Dictionary<int, AbnormalityDeterminator>();
+            return false;
+        }
+    }
+
+    private class AlwaysInfiniteResource
+    {
+        private static FieldInfo _rmulField = AccessTools.Field(typeof(GameDesc), nameof(GameDesc.resourceMultiplier));
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(GameDesc), "isInfiniteResource", MethodType.Getter)]
+        private static IEnumerable<CodeInstruction> ForceInfiniteResource(IEnumerable<CodeInstruction> instructions)
+        {
+            yield return new CodeInstruction(OpCodes.Ldc_I4, 1);
+            yield return new CodeInstruction(OpCodes.Ret);
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(FactorySystem), "GameTick", typeof(long), typeof(bool))]
+        [HarmonyPatch(typeof(FactorySystem), "GameTick", typeof(long), typeof(bool), typeof(int), typeof(int),
+            typeof(int))]
+        [HarmonyPatch(typeof(UIMinerWindow), "_OnUpdate")]
+        [HarmonyPatch(typeof(UIVeinCollectorPanel), "_OnUpdate")]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_R4 && instruction.operand.Equals(99.5f))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 0f);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,26 +1,28 @@
 ﻿# DSP Mods by Soar Qin
 
-## [DevShortcuts](DevShortcuts)
-### Restore Developer Mode keyboard Shortcuts hidden in original game codes, as following:
+## [CheatEnabler](CheatEnabler)
+### Enable cheat functions as below
+* Disable abnormal determinants (Allow get achievements on using Console and DevShortcuts)
 * Shift+F4 to switch Developer Mode on/off (no message, tip or sounds on switch).
-* Numpad 1: Gets all items and extends bag.
-* Numpad 2: Boosts walk speed, gathering speed and mecha energy restoration.
-* Numpad 3: Fills planet with foundations and bury all veins.
-* Numpad 4: +1 construction drone.
-* Numpad 5: Upgrades drone engine tech to full.
-* Numpad 6: Unlocks researching tech.
-* Numpad 7: Unlocks Drive Engine 1.
-* Numpad 8: Unlocks Drive Engine 2 and maximize energy.
-* Numpad 9: Unlocks ability to warp.
-* Numpad 0: No costs for Logistic Storages' output.
-* LCtrl + T: Unlocks all techs (not upgrades)
-* LCtrl + A: Resets all local achievements.
-* LCtrl + Q: Adds 10000 to every metadata.
-* LCtrl + W: Enters Sandbox Mode.
-* Numpad *: Proliferates items on hand.
-* Numpad /: Removes proliferations from items on hand.
-* PageDown: Remembers Pose of game camera.
-* PageUp: Locks game camera using remembered Pose.
+  * Numpad 1: Gets all items and extends bag.
+  * Numpad 2: Boosts walk speed, gathering speed and mecha energy restoration.
+  * Numpad 3: Fills planet with foundations and bury all veins.
+  * Numpad 4: +1 construction drone.
+  * Numpad 5: Upgrades drone engine tech to full.
+  * Numpad 6: Unlocks researching tech.
+  * Numpad 7: Unlocks Drive Engine 1.
+  * Numpad 8: Unlocks Drive Engine 2 and maximize energy.
+  * Numpad 9: Unlocks ability to warp.
+  * Numpad 0: No costs for Logistic Storages' output.
+  * LCtrl + T: Unlocks all techs (not upgrades)
+  * LCtrl + A: Resets all local achievements.
+  * LCtrl + Q: Adds 10000 to every metadata.
+  * LCtrl + W: Enters Sandbox Mode.
+  * Numpad *: Proliferates items on hand.
+  * Numpad /: Removes proliferations from items on hand.
+  * PageDown: Remembers Pose of game camera.
+  * PageUp: Locks game camera using remembered Pose.
+* Always infinite resource
 
 
 ## [LogisticMiner](LogisticMiner)
