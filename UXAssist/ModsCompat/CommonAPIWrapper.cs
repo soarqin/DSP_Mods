d8fa46d4953a04127169367e68eacdcba0e8255b
Soar Qin <soarchin@gmail.com>
2025-11-11 23:18:26 +0800

    priority fix

--- UXAssist/ModsCompat/CommonAPIWrapper.cs
+++ UXAssist/ModsCompat/CommonAPIWrapper.cs
@@ -10,7 +10,7 @@ public static class CommonAPIWrapper
     {
         if (!Chainloader.PluginInfos.TryGetValue(CommonAPIPlugin.GUID, out var commonAPIPlugin) ||
             commonAPIPlugin.Metadata.Version > new System.Version(1, 6, 7, 0)) return;
-        harmony.Patch(AccessTools.Method(typeof(GameOption), nameof(GameOption.InitKeys)), new HarmonyMethod(typeof(CommonAPIWrapper), nameof(PatchInitKeys)));
+        harmony.Patch(AccessTools.Method(typeof(GameOption), nameof(GameOption.InitKeys)), new HarmonyMethod(AccessTools.Method(typeof(CommonAPIWrapper), nameof(PatchInitKeys)), Priority.First));
     }
 
     public static bool PatchInitKeys(GameOption __instance)
